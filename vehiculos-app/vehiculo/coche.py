b892d05e28b50436da227a25ede8e853c5ed6ebc
Javier Alcaraz <jalcfab2411@gmail.com>
2026-04-09 11:40:36 +0200

    Formato descripción alumno A

--- vehiculos-app/vehiculo/coche.py
+++ vehiculos-app/vehiculo/coche.py
@@ -9,8 +9,7 @@ class Coche(Vehiculo):
     #    return f"{super().descripcion()} - {self.puertas} puertas"
 
     def descripcion(self):
-        base = super().descripcion()
-        return f"Coche: {base} ({self.puertas} puertas)"
+        return f"Vehículo tipo coche - {self.marca}, {self.modelo}, {self.num_puertas} puertas"
 
 
 
